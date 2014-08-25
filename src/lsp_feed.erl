0f23150f1889d3b7808d3c7c67a5d3414aa74125
Martin Winter <mwinter@netdef.org>
2014-08-25 16:27:00 -0700

    Updated License Header in source file to show GPL V2+ and commercial licensing option

--- src/lsp_feed.erl
+++ src/lsp_feed.erl
@@ -5,6 +5,36 @@
 %%%
 %%% lsp_feed - provides a feed of LSP updates
 %%%
+%%% This file is part of AutoISIS.
+%%%
+%%% License:
+%%% AutoISIS can be used (at your option) under the following GPL or under
+%%% a commercial license
+%%% 
+%%% Choice 1: GPL License
+%%% AutoISIS is free software; you can redistribute it and/or modify it
+%%% under the terms of the GNU General Public License as published by the
+%%% Free Software Foundation; either version 2, or (at your option) any
+%%% later version.
+%%% 
+%%% AutoISIS is distributed in the hope that it will be useful, but
+%%% WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+%%% the GNU General Public License for more details.
+%%% 
+%%% You should have received a copy of the GNU General Public License
+%%% along with GNU Zebra; see the file COPYING.  If not, write to the Free
+%%% Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+%%% 02111-1307, USA.
+%%% 
+%%% Choice 2: Commercial License Usage
+%%% Licensees holding a valid commercial AutoISIS may use this file in 
+%%% accordance with the commercial license agreement provided with the 
+%%% Software or, alternatively, in accordance with the terms contained in 
+%%% a written agreement between you and the Copyright Holder.  For
+%%% licensing terms and conditions please contact us at 
+%%% licensing@netdef.org
+%%%
 %%% @end
 %%% Created : 5 July 2014 by Rick Payne <rickp@rossfell.co.uk>
 %%%-------------------------------------------------------------------
